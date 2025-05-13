0a86ba8501d1a4f7c3f62a5ac3fafb786c7a323a
FelixNievelstein <lixstein@gmx.de>
2025-05-13 16:05:55 +0200

    lights on;

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
 from pybricks.parameters import Port, Color
 from pybricks.tools import wait
-# from .greifen import oeffnen
+from pybricks.robotics import DriveBase
 
 hub = PrimeHub()
 
@@ -10,9 +10,48 @@ hub = PrimeHub()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
+greif_motor = Motor(Port.A)
+abstandssensor = UltrasonicSensor(Port.D)
+lightOn = True
+
+drive_base = DriveBase(left_motor, right_motor, 56, 114)
+
 # Farbsensor
 sensor = ColorSensor(Port.E)
 shouldRun = True 
+
+def grab():
+    reset_motor()
+    move_down()
+    run_task(drive_forward())
+    move_up()
+
+def drop():
+    move_down()
+    wait(1000)
+    run_task(drive_backward())
+    reset_motor()
+
+def reset_motor():
+    print("Reset")
+    greif_motor.run_target(100, 0)
+    wait(1000)
+
+def move_up():
+    print("Up")
+    greif_motor.run_target(1000, 20, then=Stop.HOLD)
+
+def move_down():
+    print("Down")
+    greif_motor.run_target(100, -50, then=Stop.HOLD)
+
+async def drive_forward():
+    await drive_base.straight(50)
+    wait(1000)
+
+async def drive_backward():
+    await drive_base.straight(-50)
+    wait(1000)
 
 class Driver:
 
@@ -51,6 +90,7 @@ class Driver:
                 
         elif color == Color.YELLOW:
             print("Stop Gelb")     
+            drop()
             self.hub.display.text("Juhu")
             self.run = False       
         elif color == Color.WHITE:
@@ -72,6 +112,7 @@ class Driver:
             self.whiteCount += 1
             wait(100)
         else:
+            grab()
             self.left_motor.run(self.base_speed * 20)
             self.right_motor.run(-self.base_speed * 20)
             self.direction = self.direction * -1
@@ -85,4 +126,5 @@ while shouldRun:
         sensor.detectable_colors([Color.BLACK, Color.WHITE, Color.MAGENTA, Color.VIOLET, Color.YELLOW, Color.BLUE])
         color = sensor.color()
         driver.calcMoving(color)
+        abstandssensor.lights.on()
         # Drehgeschwindigkeit setzen (in Grad/Sek)
