03e02e803c2802d53f937787fe06dd8ebc0bef59
FelixNievelstein <lixstein@gmx.de>
2025-05-13 14:25:21 +0200

    drive on line;

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pybricks.pupdevices import Motor, ColorSensor
 from pybricks.parameters import Port, Color
 from pybricks.tools import wait
 
-
 hub = PrimeHub()
 
 # Motoren
@@ -12,46 +11,44 @@ right_motor = Motor(Port.C)
 
 # Farbsensor
 sensor = ColorSensor(Port.E)
-sensor.lights.off()
 
-# Reflektionswerte
-BLACK = 15
-WHITE = 98
-LILA = 30
-YELLOW = 89
-BLUE = 57
-THRESHOLD = (BLACK + WHITE) / 2
+class Driver:
 
-Kp = 1.0
-base_speed = 50
-whiteCount = 0
-direction = 1
-reflection1 = 0
-reflection2 = 0
-reflection3 = 0
-reflection4 = 0
-reflection5 = 0
+    base_speed = 250
+    whiteCount = 0
+    direction = 1
 
-def calcMoving(color):
-    print(color)
-    if color == Color.BLACK: 
-        direction = 1
-        left_motor.run(-base_speed)
-        right_motor.run(base_speed)
-    elif color == Color.YELLOW:
-        print("Stop Gelb")   
-    elif color == Color.WHITE:
-        if whiteCount == 5:
-            direction = -1            
-            print("change direction")
+    def __init__(self, left_motor, right_motor):
+        self.left_motor = left_motor
+        self.right_motor = right_motor
 
-        left_motor.run(base_speed - direction)
-        right_motor.run(base_speed + direction)
-        whiteCount += 1
+
+    def calcMoving(self, color):
+        print(color)
+        if color == Color.BLACK: 
+            self.base_speed = 250
+            direction = 1
+            self.left_motor.run(-self.base_speed)
+            self.right_motor.run(self.base_speed)
+        elif color == Color.YELLOW:
+            print("Stop Gelb")
+        elif color == Color.WHITE:
+            self.base_speed = 5
+            if self.whiteCount == 50:
+                self.direction = self.direction * -1
+                print("change direction")
+                self.whiteCount = 0
+
+            self.left_motor.run(self.base_speed * self.direction)
+            self.right_motor.run(self.base_speed * self.direction)
+            self.whiteCount += 1
+            wait(100)
+
+driver = Driver(left_motor, right_motor)
+
 
 while True:
-    sensor.detectable_colors([Color.BLACK, Color.WHITE, Color.MAGENTA, Color.VIOLET, Color.YELLOW, Color.BLUE])
-    color = sensor.color()
-    # Drehgeschwindigkeit setzen (in Grad/Sek)
-
-    wait(10)
+        sensor.detectable_colors([Color.BLACK, Color.WHITE, Color.MAGENTA, Color.VIOLET, Color.YELLOW, Color.BLUE])
+        color = sensor.color()
+        driver.calcMoving(color)
+        # Drehgeschwindigkeit setzen (in Grad/Sek)
