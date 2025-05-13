1b54074327cb3c1a6c7c9f26885cd9157f60ff3c
FelixNievelstein <lixstein@gmx.de>
2025-05-13 13:09:23 +0200

    detect lila

--- main2.py
+++ main2.py
@@ -10,7 +10,13 @@ hub = PrimeHub()
 sensor = ColorSensor(Port.E)
 #my_lila = Color()
 
+OUR_BLUE = (46, Color.BLUE)
+OUR_LILA = (26, Color.BLUE)
+OUR_BLACK = ()
+
 
 while True:     
-    sensor.detectable_colors([Color.BLACK, Color.WHITE, Color.MAGENTA, Color.VIOLET, Color.YELLOW, Color.BLUE])
-    print(sensor.color())
+    #sensor.detectable_colors([Color.BLACK, Color.WHITE, Color.MAGENTA, Color.YELLOW, Color.BLUE])
+    ref = sensor.reflection()
+    color = sensor.color()
+    print(ref, color)
